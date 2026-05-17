80956b1bbb84b7a6ea823e00cbd3dc66d3faf1a2
AlbertoV01 <albertovlzlar_@hotmail.com>
2026-05-17 15:12:00 -0600

    Agregar Workflows de GitHub Actions

--- obj/Debug/net9.0/portafolioV2.AssemblyInfo.cs
+++ obj/Debug/net9.0/portafolioV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600fb5e7407361c20f11da1e2ac0196e37e31a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
