b8fbdd93d5d363cbae30cfbf4968dec4f44a1bf0
AlbertoV01 <albertovlzlar_@hotmail.com>
2026-05-21 01:45:12 -0600

    correccion

--- obj/Debug/net9.0/portafolioV2.AssemblyInfo.cs
+++ obj/Debug/net9.0/portafolioV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d91d1142ed8542004dbb9bcd973aa4b4916971e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324e7f5c9f4c641e9e123f454735e1c8b82a7843")]
 [assembly: System.Reflection.AssemblyProductAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portafolioV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
